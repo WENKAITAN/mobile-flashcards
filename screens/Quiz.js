2aa647b858de9d4717c3223a733246c38f430e1a
WenakiTan <k9162477@icloud.com>
2020-04-29 10:20:38 -0400

    1

--- screens/Quiz.js
+++ screens/Quiz.js
@@ -1,29 +1,257 @@
 import React, { Component } from 'react';
-import Constants from 'expo-constants';
-import Quiz_Android from './Quiz_android';
-import Quiz_iOS from './Quiz_ios';
+import { View, Text, StyleSheet, ScrollView, Dimensions } from 'react-native';
+import TextButton from './TextButton';
+import TouchButton from './TouchButton';
+import { gray, green, red, textGray, darkGray, white } from '../utils/colors';
+import { connect } from 'react-redux';
 import { clearNotification, setLocalNotification } from '../utils/helper';
 
-export class Quiz extends Component {
-    // componentDidMount(){
-    //     clearNotification().then(setLocalNotification)
-    // }
-    static navigationOptions = () => {
-        const { route } = this.props;
-        const title = route.params.title
-        return {
-            title: `${title} Quiz`
-        };
+const screen = {
+    QUESTION: 'question',
+    ANSWER: 'answer',
+    RESULT: 'result'
+};
+const answer = {
+    CORRECT: 'correct',
+    INCORRECT: 'incorrect'
+};
+const SCREEN_WIDTH = Dimensions.get('window').width;
+
+class Quiz extends Component {
+
+    state = {
+        show: screen.QUESTION,
+        correct: 0,
+        incorrect: 0,
+        questionCount: this.props.deck.questions.length,
+        answered: Array(this.props.deck.questions.length).fill(0)
+    };
+    handleScroll = () => {
+        this.setState({
+            show: screen.QUESTION
+        });
+    };
+    handleAnswer = (response, page) => {
+        if (response === answer.CORRECT) {
+            this.setState(prevState => ({ correct: prevState.correct + 1 }));
+        } else {
+            this.setState(prevState => ({ incorrect: prevState.incorrect + 1 }));
+        }
+        this.setState(
+            prevState => ({
+                answered: prevState.answered.map((val, idx) => (page === idx ? 1 : val))
+            }),
+            () => {
+                const { correct, incorrect, questionCount } = this.state;
+
+                if (questionCount === correct + incorrect) {
+                    this.setState({ show: screen.RESULT });
+                } else {
+                    this.scrollView.scrollTo({ x: (page + 1) * SCREEN_WIDTH });
+                    this.setState(prevState => ({
+                        show: screen.QUESTION
+                    }));
+                }
+            }
+        );
+    };
+    handleReset = () => {
+        this.setState(prevState => ({
+            show: screen.QUESTION,
+            correct: 0,
+            incorrect: 0,
+            answered: Array(prevState.questionCount).fill(0)
+        }));
+        clearNotification()
+         .then(setLocalNotification)
     };
     render() {
-        const { route } = this.props;
-        const title = route.params.title
+        const { questions } = this.props.deck;
+        const { show } = this.state;
 
-        if (Constants.platform.android) {
-            return <Quiz_Android title={title} />;
+        if (questions.length === 0) {
+            return (
+                <View style={styles.pageStyle}>
+                    <View style={styles.block}>
+                        <Text style={[styles.count, { textAlign: 'center' }]}>
+                            You cannot take a quiz because there are no cards in the deck.
+            </Text>
+                        <Text style={[styles.count, { textAlign: 'center' }]}>
+                            Please add some cards and try again.
+            </Text>
+                    </View>
+                </View>
+            );
         }
-        return <Quiz_iOS title={title} />;
+
+        if (this.state.show === screen.RESULT) {
+            const { correct, questionCount } = this.state;
+            const percent = ((correct / questionCount) * 100).toFixed(0);
+            const resultStyle =
+                percent >= 70 ? styles.resultTextGood : styles.resultTextBad;
+
+            return (
+                <View style={styles.pageStyle}>
+                    <View style={styles.block}>
+                        <Text style={styles.count}>Done</Text>
+                    </View>
+                    <View style={styles.block}>
+                        <Text style={[styles.count, { textAlign: 'center' }]}>
+                            Quiz Complete!
+            </Text>
+                        <Text style={resultStyle}>
+                            {correct} / {questionCount} correct
+            </Text>
+                    </View>
+                    <View style={styles.block}>
+                        <Text style={[styles.count, { textAlign: 'center' }]}>
+                            Percentage correct
+            </Text>
+                        <Text style={resultStyle}>{percent}%</Text>
+                    </View>
+                    <View>
+                        <TouchButton
+                            btnStyle={{ backgroundColor: green, borderColor: white }}
+                            onPress={this.handleReset}
+                        >
+                            Restart Quiz
+            </TouchButton>
+                    </View>
+                </View>
+            );
+        }
+
+        return (
+            <ScrollView
+                style={styles.container}
+                pagingEnabled={true}
+                horizontal={true}
+                onMomentumScrollBegin={this.handleScroll}
+                ref={scrollView => {
+                    this.scrollView = scrollView;
+                }}
+            >
+                {questions.map((question, idx) => (
+                    <View style={styles.pageStyle} key={idx}>
+                        <View style={styles.block}>
+                            <Text style={styles.count}>
+                                {idx + 1} / {questions.length}
+                            </Text>
+                        </View>
+                        <View style={[styles.block, styles.questionContainer]}>
+                            <Text style={styles.questionText}>
+                                {show === screen.QUESTION ? 'Question' : 'Answer'}
+                            </Text>
+                            <View style={styles.questionWrapper}>
+                                <Text style={styles.title}>
+                                    {show === screen.QUESTION
+                                        ? question.question
+                                        : question.answer}
+                                </Text>
+                            </View>
+                        </View>
+                        {show === screen.QUESTION ? (
+                            <TextButton
+                                txtStyle={{ color: red }}
+                                onPress={() => this.setState({ show: screen.ANSWER })}
+                            >
+                                Answer
+                            </TextButton>
+                        ) : (
+                                <TextButton
+                                    txtStyle={{ color: red }}
+                                    onPress={() => this.setState({ show: screen.QUESTION })}
+                                >
+                                    Question
+                                </TextButton>
+                            )}
+                        <View>
+                            <TouchButton
+                                btnStyle={{ backgroundColor: green, borderColor: white }}
+                                onPress={() => this.handleAnswer(answer.CORRECT, idx)}
+                                disabled={this.state.answered[idx] === 1}
+                            >
+                                Correct
+              </TouchButton>
+                            <TouchButton
+                                btnStyle={{ backgroundColor: red, borderColor: white }}
+                                onPress={() => this.handleAnswer(answer.INCORRECT, idx)}
+                                disabled={this.state.answered[idx] === 1}
+                            >
+                                Incorrect
+              </TouchButton>
+                        </View>
+                    </View>
+                ))}
+            </ScrollView>
+        );
     }
 }
 
-export default Quiz;
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    pageStyle: {
+        flex: 1,
+        paddingTop: 16,
+        paddingLeft: 16,
+        paddingRight: 16,
+        paddingBottom: 16,
+        backgroundColor: gray,
+        justifyContent: 'space-around',
+        width: SCREEN_WIDTH
+    },
+    block: {
+        marginBottom: 20
+    },
+    count: {
+        fontSize: 24
+    },
+    title: {
+        fontSize: 32,
+        textAlign: 'center'
+    },
+    questionContainer: {
+        borderWidth: 1,
+        borderColor: darkGray,
+        backgroundColor: white,
+        borderRadius: 5,
+        paddingTop: 20,
+        paddingBottom: 20,
+        paddingLeft: 16,
+        paddingRight: 16,
+        flexGrow: 1
+    },
+    questionWrapper: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    questionText: {
+        textDecorationLine: 'underline',
+        textAlign: 'center',
+        fontSize: 20
+    },
+    resultTextGood: {
+        color: green,
+        fontSize: 46,
+        textAlign: 'center'
+    },
+    resultTextBad: {
+        color: red,
+        fontSize: 46,
+        textAlign: 'center'
+    }
+});
+
+const mapStateToProps = (state, prop) => {
+    const deck = state[prop.route.params.title];
+
+    return {
+        deck
+    };
+};
+
+export default connect(mapStateToProps)(Quiz)
